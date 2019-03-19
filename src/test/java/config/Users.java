9527cce3abe5724ceec9f8352d5ea95a8bb15a33
Samuel de Paula Pereira <samuelpereira.qa@outlook.com>
2019-03-19 15:14:39 -0300

    Update Users.java

--- src/test/java/config/Users.java
+++ src/test/java/config/Users.java
@@ -1,6 +1,6 @@
 package config;
 
 public class Users {
-    public static final String UserLogin = "helio@vendedor.com";
-    public static final String PasswordLogin = "ps654321";
+    public static final String UserLogin = "usuario@email.com";
+    public static final String PasswordLogin = "senhausuario";
 }
